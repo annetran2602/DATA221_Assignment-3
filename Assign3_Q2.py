a435f29675566859a051f4804ba44d6a5d092a75
annetran2602 <anne.tran2@ucalgary.ca>
2026-02-13 17:44:38 -0700

    Assign2_Q2: Create plots: histogram, box plot and describing how the data distribute and identify the outlier.

--- Assign3_Q2.py
+++ Assign3_Q2.py
@@ -18,6 +18,11 @@ ax[1].set_title("Box Plot of Violent Crimes Per Population")
 
 plt.show()
 
+# Answer the questions-------------------------------------------------------------------------
+# Histogram divide the range of values into a number of bins and reflect the frequency of each bin. According to this histogram, this is right-skewed histogram because it shows the peak values on the left side of the histogram and lower values are on the right side.
+# Box plot summary the median value, max and min values, 25th, 50th, 75th percentile of data. The middle line in the box determine the median value which is ~0.385 . Lower bound of the box determine the 25th percentile which is ~0.2 and 75th percentile is ~0.645 which is show on the top bound of the box. Two handle of the box determine the max and values are ~0.017 and ~0.995.
+# Generally, box plot can show the outliers like a dot, but it not show in this box plot.
+
 
 
 
